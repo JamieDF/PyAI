3677c4c2db38ae3b9fa925b50a0be7efa790d370
Jamie Fraser <jamie_fraser@rocketmail.com>
2016-10-17 22:45:51 +0100

    breakdown

--- Astar.py
+++ Astar.py
@@ -1,5 +1,5 @@
 from implementation import *
-came_from, cost_so_far = a_star_search(diagram4, (1, 4), (7, 8))
-draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+came_from, cost_so_far = a_star_search(diagram4, (0, 0), (5, 2))
+draw_grid(diagram4, width=3, point_to=came_from, start=(0, 0), goal=(5, 2))
 print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+draw_grid(diagram4, width=3, number=cost_so_far, start=(0, 0), goal=(5, 2))
--- implementation.py
+++ implementation.py
@@ -1,41 +1,3 @@
-# Sample code from http://www.redblobgames.com/pathfinding/
-# Copyright 2014 Red Blob Games <redblobgames@gmail.com>
-#
-# Feel free to use this code in your own projects, including commercial projects
-# License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
-
-class SimpleGraph:
-    def __init__(self):
-        self.edges = {}
-    
-    def neighbors(self, id):
-        return self.edges[id]
-
-example_graph = SimpleGraph()
-example_graph.edges = {
-    'A': ['B'],
-    'B': ['A', 'C', 'D'],
-    'C': ['A'],
-    'D': ['E', 'A'],
-    'E': ['B']
-}
-
-import collections
-
-class Queue:
-    def __init__(self):
-        self.elements = collections.deque()
-    
-    def empty(self):
-        return len(self.elements) == 0
-    
-    def put(self, x):
-        self.elements.append(x)
-    
-    def get(self):
-        return self.elements.popleft()
-
-# utility functions for dealing with square grids
 def from_id_width(id, width):
     return (id % width, id // width)
 
@@ -49,9 +11,9 @@ def draw_tile(graph, id, style, width):
         if x2 == x1 - 1: r = "\u2190"
         if y2 == y1 + 1: r = "\u2193"
         if y2 == y1 - 1: r = "\u2191"
-    if 'start' in style and id == style['start']: r = "A"
-    if 'goal' in style and id == style['goal']: r = "Z"
-    if 'path' in style and id in style['path']: r = "@"
+    if 'start' in style and id == style['start']: r = "A "
+    if 'goal' in style and id == style['goal']: r = "Z "
+    if 'path' in style and id in style['path']: r = "@ "
     if id in graph.walls: r = "#" * width
     return r
 
@@ -93,8 +55,8 @@ class GridWithWeights(SquareGrid):
     def cost(self, from_node, to_node):
         return self.weights.get(to_node, 1)
 
-diagram4 = GridWithWeights(10, 10)
-diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+diagram4 = GridWithWeights(8, 5)
+diagram4.walls = [(0, 2), (2, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3)]
 diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
                                        (4, 3), (4, 4), (4, 5), (4, 6), 
                                        (4, 7), (4, 8), (5, 1), (5, 2),
@@ -117,30 +79,6 @@ class PriorityQueue:
     
     def get(self):
         return heapq.heappop(self.elements)[1]
-
-def dijkstra_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not frontier.empty():
-        current = frontier.get()
-        
-        if current == goal:
-            break
-        
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next, priority)
-                came_from[next] = current
-    
-    return came_from, cost_so_far
 
 def reconstruct_path(came_from, start, goal):
     current = goal
