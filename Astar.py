a70dbca3016318c5b5348ad11b34ede76dc38a04
JamieDF <jamie_fraser@rocketmail.com>
2016-10-18 16:39:28 +0100

    gets user goal and start (x,y)

--- Astar.py
+++ Astar.py
@@ -1,10 +1,16 @@
 
+#TODO Multiple Robots
+
+
+start = tuple(map(int, input("Please enter the robots start location(x,y) :").split(',')))
+goal = tuple(map(int, input("Please enter the robots goal location(x,y) :").split(',')))
+
 print ("\n Key : \n R = robot location \n G = Goal \n T = Trap \n ### = Wall \n @ = final path to goal \n")
-print("_________________________")
+print("-----------------------------------")
 from implementation import *
-came_from, cost_so_far = a_star_search(map, (0, 0), (10, 9))
-draw_grid(map, width=3, point_to=came_from, start=(0, 0), goal=(10, 9))
-print("_________________________")
-draw_grid(map, width=3, number=cost_so_far, start=(0, 0), goal=(10, 9))
-print("_________________________")
-draw_grid(map, width=3, path=reconstruct_path(came_from, start=(0, 0), goal=(10, 9)))
+came_from, cost_so_far = a_star_search(map, start, goal)
+draw_grid(map, width=3, point_to=came_from, start=start, goal=goal)
+print("-----------------------------------")
+draw_grid(map, width=3, number=cost_so_far, start=start, goal=goal)
+print("-----------------------------------")
+draw_grid(map, width=3, path=reconstruct_path(came_from, start=start, goal=goal))
