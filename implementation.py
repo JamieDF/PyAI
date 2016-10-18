0c162166e5c926c1b1f3b9c70eef49d9ce1343df
JamieDF <jamie_fraser@rocketmail.com>
2016-10-18 12:54:16 +0100

    output a bit nicer

--- implementation.py
+++ implementation.py
@@ -3,14 +3,14 @@ def from_id_width(id, width):
 
 def draw_tile(graph, id, style, width):
     r = " ."
-    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
+    if 'number' in style and id in style['number']: r = " %d" % style['number'][id]
     if 'point_to' in style and style['point_to'].get(id, None) is not None:
         (x1, y1) = id
         (x2, y2) = style['point_to'][id]
-        if x2 == x1 + 1: r = "\u2190"
-        if x2 == x1 - 1: r = "\u2192"
-        if y2 == y1 + 1: r = "\u2191"
-        if y2 == y1 - 1: r = "\u2193"
+        if x2 == x1 + 1: r = " \u2190"
+        if x2 == x1 - 1: r = " \u2192"
+        if y2 == y1 + 1: r = " \u2191"
+        if y2 == y1 - 1: r = " \u2193"
 
        #Orignal Arrow directions
        #if x2 == x1 + 1: r = "\u2192"
@@ -18,11 +18,11 @@ def draw_tile(graph, id, style, width):
        #if y2 == y1 + 1: r = "\u2193"
        #if y2 == y1 - 1: r = "\u2191"
 
-    if 'start' in style and id == style['start']: r = "A"
-    if 'goal' in style and id == style['goal']: r = "Z"
-    if 'path' in style and id in style['path']: r = "@"
-    if id in graph.walls: r = "W" * width
-    if id in graph.trap: r = "T"  #added trap
+    if 'start' in style and id == style['start']: r = " A"
+    if 'goal' in style and id == style['goal']: r = " Z"
+    if 'path' in style and id in style['path']: r = " @"
+    if id in graph.walls: r = "#" * width
+    if id in graph.trap: r = " T"  #added trap
     return r
 
 def draw_grid(graph, width=2, **style):
