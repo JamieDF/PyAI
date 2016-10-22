ab78a3fbf6715f3b9e193c6e9b5d25f33ea2222c
Jamie Fraser <jamie_fraser@rocketmail.com>
2016-10-22 20:09:03 +0100

    layout

--- implementation.py
+++ implementation.py
@@ -1,6 +1,7 @@
 def from_id_width(id, width):
     return (id % width, id // width)
 
+
 def draw_tile(graph, id, style, width):
     r = " ."
     if 'number' in style and id in style['number']: r = " %d" % style['number'][id]
@@ -16,8 +17,9 @@ def draw_tile(graph, id, style, width):
     if 'goal' in style and id == style['goal']: r = " G"
     if 'path' in style and id in style['path']: r = " @"
     if id in graph.walls: r = "#" * width
-    if id in graph.trap: r = " T"  #added trap
+    if id in graph.trap: r = " T"  # added trap
     return r
+
 
 def draw_grid(graph, width=2, **style):
     for y in range(graph.height):
@@ -32,36 +34,40 @@ class SquareGrid:
         self.height = height
         self.walls = []
         self.trap = []
-    
+
     def in_bounds(self, id):
         (x, y) = id
         return 0 <= x < self.width and 0 <= y < self.height
-    
+
     def passable(self, id):
         return id not in self.walls
-    
+
     def neighbors(self, id):
         (x, y) = id
-        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
-        if (x + y) % 2 == 0: results.reverse() # aesthetics
+        results = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+        if (x + y) % 2 == 0: results.reverse()  # aesthetics
         results = filter(self.in_bounds, results)
         results = filter(self.passable, results)
         return results
+
 
 class GridWithWeights(SquareGrid):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.weights = {}
-    
+
     def cost(self, from_node, to_node):
         return self.weights.get(to_node, 1)
 
+
 map = GridWithWeights(12, 12)
-map.walls = [(4, 2), (5, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3), (4, 2), (1, 7), (11, 4), (3, 11), (4, 10), (9, 5), (6, 6), (0, 4), (2, 4)]
+map.walls = [(4, 2), (5, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3), (4, 2), (1, 7), (11, 4), (3, 11), (4, 10),
+             (9, 5), (6, 6), (0, 4), (2, 4)]
 map.trap = [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]
 map.weights = {loc: 5 for loc in [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]}
 
 import heapq
+
 
 class PriorityQueue:
     def __init__(self):
@@ -76,20 +82,23 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.elements)[1]
 
+
 def reconstruct_path(came_from, start, goal):
     current = goal
     path = [current]
     while current != start:
         current = came_from[current]
         path.append(current)
-    path.append(start) # optional
-    path.reverse() # optional
+    path.append(start)  # optional
+    path.reverse()  # optional
     return path
+
 
 def heuristic(a, b):
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
+
 
 def a_star_search(graph, start, goal):
     frontier = PriorityQueue()
@@ -98,13 +107,13 @@ def a_star_search(graph, start, goal):
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
-    
+
     while not frontier.empty():
         current = frontier.get()
-        
+
         if current == goal:
             break
-        
+
         for next in graph.neighbors(current):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
@@ -112,5 +121,5 @@ def a_star_search(graph, start, goal):
                 priority = new_cost + heuristic(goal, next)
                 frontier.put(next, priority)
                 came_from[next] = current
-    
+
     return came_from, cost_so_far
