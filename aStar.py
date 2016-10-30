dff6f43d2ba3477553c31b67149ac4ea4f1bf598
Jamie Fraser <jamie_fraser@rocketmail.com>
2016-10-30 14:30:07 +0000

    something fucked up,  fixed now though

--- aStar.py
+++ aStar.py
@@ -5,12 +5,13 @@ from implementation import GridWithWeights
 start = tuple(map(int, input("Please enter the robots start location(x,y) :").split(',')))
 goal = tuple(map(int, input("Please enter the robots goal location(x,y) :").split(',')))
 
-readWalls = open('GridFiles/gridWalls.txt', 'r')
+"""readWalls = open('GridFiles/gridWalls.txt', 'r')
 gridWalls = readWalls.read()
 print(gridWalls)
-print(type(gridWalls))
+print(type(gridWalls))"""
+
 map = GridWithWeights(12, 12)
-map.walls = [gridWalls]
+map.walls = [(0, 2), (2, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3)]
 map.trap = [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]
 map.weights = {loc: 5 for loc in [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]}
 
--- implementation.py
+++ implementation.py
@@ -1,5 +1,6 @@
 import math
 
+
 def from_id_width(id, width):
     return (id % width, id // width)
 
@@ -61,21 +62,14 @@ class GridWithWeights(SquareGrid):
     def cost(self, from_node, to_node):
         return self.weights.get(to_node, 1)
 
-<<<<<<< HEAD
 
-map = GridWithWeights(12, 12)
-map.walls = [(4, 2), (5, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3), (4, 2), (1, 7), (11, 4), (3, 11), (4, 10),
-             (9, 5), (6, 6), (0, 4), (2, 4)]
-=======
 # Map details and locations
 '''
 map = GridWithWeights(12, 12)
 map.walls = [(1, 4),  (4, 2), (5, 4), (3, 4), (3, 3), (3, 1), (4, 1), (5, 3), (7, 3), (4, 2), (1, 7), (11, 4), (3, 11), (4, 10), (9, 5), (6, 6), (0, 4), (2, 4)]
->>>>>>> b5a4564057b5b801144ccb97ddfc0064d4566f19
 map.trap = [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]
 map.weights = {loc: 5 for loc in [(3, 2), (8, 8), (7, 7), (2, 7), (11, 11)]}
 '''
-
 
 import heapq
 
@@ -104,33 +98,22 @@ def reconstruct_path(came_from, start, goal):
     path.reverse()  # optional
     return path
 
-<<<<<<< HEAD
 
-def heuristic(a, b):
-=======
 def manhattanHeuristic(a, b):
->>>>>>> b5a4564057b5b801144ccb97ddfc0064d4566f19
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
 
-<<<<<<< HEAD
-=======
+
 def euclidianHeuristic(a, b):
     (x1, y1) = a
     (x2, y2) = b
-    return abs(math.sqrt((abs(x1 - x2))**2 + (abs(y1 - y2))**2))
->>>>>>> b5a4564057b5b801144ccb97ddfc0064d4566f19
+    return abs(math.sqrt((abs(x1 - x2)) ** 2 + (abs(y1 - y2)) ** 2))
+
 
 def a_star_search(graph, start, goal):
     heur = int(input("Type '1' to execute with Manhattan heuristic OR '2' to for Euclidian\n->"))
-<<<<<<< HEAD
     while (heur != 1) and (heur != 2):
-=======
-    print(heur)
-    print(type(heur))
-    while (heur != 1) or (heur != 2):
->>>>>>> 19d7cab660a6abe8360dfb57743e0026fcea10ce
         heur = int(input("Type '1' to execute with Manhattan heuristic OR '2' to for Euclidian\n->"))
     frontier = PriorityQueue()
     frontier.put(start, 0)
@@ -138,31 +121,14 @@ def a_star_search(graph, start, goal):
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
-<<<<<<< HEAD
 
-    while not frontier.empty():
-        current = frontier.get()
-
-        if current == goal:
-            break
-
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
-=======
-    
     if (heur == 1):
         while not frontier.empty():
             current = frontier.get()
-        
+
             if current == goal:
                 break
-        
-<<<<<<< HEAD
+
             for next in graph.neighbors(current):
                 new_cost = cost_so_far[current] + graph.cost(current, next)
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
@@ -171,23 +137,13 @@ def a_star_search(graph, start, goal):
                     frontier.put(next, priority)
                     came_from[next] = current
 
-=======
-                for next in graph.neighbors(current):
-                    new_cost = cost_so_far[current] + graph.cost(current, next)
-                    if next not in cost_so_far or new_cost < cost_so_far[next]:
-                        cost_so_far[next] = new_cost
-                        priority = new_cost + manhattanHeuristic(goal, next)
-                        frontier.put(next, priority)
-                        came_from[next] = current
->>>>>>> 19d7cab660a6abe8360dfb57743e0026fcea10ce
     else:
         while not frontier.empty():
             current = frontier.get()
-        
+
             if current == goal:
                 break
-        
-<<<<<<< HEAD
+
             for next in graph.neighbors(current):
                 new_cost = cost_so_far[current] + graph.cost(current, next)
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
@@ -195,22 +151,10 @@ def a_star_search(graph, start, goal):
                     priority = new_cost + euclidianHeuristic(goal, next)
                     frontier.put(next, priority)
                     came_from[next] = current
-=======
-                for next in graph.neighbors(current):
-                    new_cost = cost_so_far[current] + graph.cost(current, next)
-                    if next not in cost_so_far or new_cost < cost_so_far[next]:
-                        cost_so_far[next] = new_cost
-                        priority = new_cost + euclidianHeuristic(goal, next)
-                        frontier.put(next, priority)
-                        came_from[next] = current
->>>>>>> 19d7cab660a6abe8360dfb57743e0026fcea10ce
 
-
-    #New,  Exits program if the robot could not make it to the goal
+    # New,  Exits program if the robot could not make it to the goal
     if current != goal:
         import sys
         sys.exit("could not find goal")
->>>>>>> b5a4564057b5b801144ccb97ddfc0064d4566f19
 
     return came_from, cost_so_far
-
